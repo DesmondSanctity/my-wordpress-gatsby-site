4d805ed282a3776070ac88cff56c4db900c34a5c
Desmond Obisi <desmondcutest@gmail.com>
2022-04-30 16:03:25 +0100

    blog setup with wp

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://wpgatsbydemo.wpengine.com/graphql`,
+          `https://bant-n-chat.online/graphql`,
       },
     },
 
